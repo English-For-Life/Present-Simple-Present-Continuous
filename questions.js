53c85e6f26026a3b4fa8cd20d572670658dadee1
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-02 22:48:59 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Ann usually ___ to school by bus.",
+        options:["go", "goes", "is going"],
+        answer: 1
+    },
+    {
+     q:"Listen! Mark ___ the violin.",
+        options:["is playing", "plays", "play"],
+        answer: 0   
+    },
+    {
+        q:"I always ___ my homework after school.",
+        options:["does", "am doing", "do"],
+        answer: 2
+    },
+    {
+        q:"They ___ TV at the moment.",
+        options:["watch", "is watching", "are watching"],
+        answer: 2
+    },
+    {
+        q:"My sister ___ music now.",
+        options:["are listening", "listens", "is listening"],
+        answer: 2
+    },
+    {
+        q:"We ___ football on Sundays.",
+        options:["are playing", "play", "plays"],
+        answer: 1
+    },
+    {
+        q:"My uncle often ___ his car.",
+        options:["drives", "drive", "is driving"],
+        answer: 0
+    },
+    {
+        q:"Where is mum? - She ___ in the kitchen. ",
+        options:["is cooking", "am cooking", "don't do"],
+        answer: 0
+    },
+    {
+        q:"Emma sometimes ___ her mum with the housework.",
+        options:["help", "helps", "is helping"],
+        answer: 1
+    },
+    {
+        q:"I ___ my teeth now.",
+        options:["brush", "is brushing", "'m brushing"],
+        answer: 2
+    },
+    {
+        q:"___ she ___ her grandma every Sunday?",
+        options:["Is / visiting", "Do / visit", "Does / visit"],
+        answer: 2
+    },
+    {
+        q:"___ you ___ computer games right now?",
+        options:["Do / play", "Is / playing", "Are / playing"],
+        answer: 2
+    },
+    {
+        q:"My brother ___ Geography.",
+        options:["doesn't likes", "doesn't like", "don't like"],
+        answer: 1
+    },
+    {
+        q:"We ___ at the moment.",
+        options:["aren't fighting", "isn't fighting", "don't fight"],
+        answer: 0
+    },
+    {
+        q:"___ your father ___ in a factory?",
+        options:["Do / work", "Does / work", "Is working"],
+        answer: 1
+    },
+    {
+        q:"She ___ English at the moment.",
+        options:["doesn't learn", "don't learn", "isn't learning"],
+        answer: 2
+    },
+    {
+        q:"I ___ the dog for a walk every day.",
+        options:["don't take", "doesn't take", "'m not taking"],
+        answer: 0
+    },
+    {
+        q:"What time ___ they ___ ?",
+        options:["does / gets up", "do / get up", "are / getting up"],
+        answer: 1
+    },
+    {
+        q:"Where ___ they ___ ? In a house or a flat?",
+        options:["are / living", "does / lives", "do / live"],
+        answer: 2
+    },
+    {
+        q:"Sam ___ now.",
+        options:["isn't running", "doesn't run", "aren't running"],
+        answer: 0
+    }
+]
